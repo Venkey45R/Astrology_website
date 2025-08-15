191d442bda502d13c51adaf937271d72bb922f89
rickievishal <rickievishalyt@gmail.com>
2025-08-15 11:19:15 +0530

    Added a cron job route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user");
 const bookingRoutes = require("./routes/bookings");
 const unavailableSlotRoutes = require("./routes/unavailableSlots");
+const awakenRoutes = require("./routes/awaken");
+
 const payuRoutes = require("./routes/payuRoutes");
 const Razorpay = require("razorpay");
 const cashfreeRoutes = require("./routes/cashfreeRoutes");
@@ -29,7 +31,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/bookings", bookingRoutes);
-
+app.use("/api/awaken",awakenRoutes)
 app.use("/api/unavailable-slots", unavailableSlotRoutes);
 app.use("/api/cashfree", cashfreeRoutes);
 
